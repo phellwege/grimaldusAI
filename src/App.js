fc93dede8052ec20367a7ccdce26c4aae2fa2b5d
Peter <phellwege1@gmail.com>
2024-04-03 14:42:34 -0600

    grimaldus gids

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import react, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Card, Form } from 'react-bootstrap';
+import React from 'react';
 
 function App() {
+  const [textInput, setTextInput] = useState(null)
   return (
     <>
     <div className='pageWrapper'>
       <h1>Grimaldus AI</h1>
-      
+      <Card className='inputAndOutputCard'>
+        <Card.Header>
+          Input choices
+        </Card.Header>
+        <Card.Body>
+          <div className='cardInnerDiv'>
+            <div className='cardLeftDiv'>
+              <Form>
+                <Form.Label>Text Input</Form.Label>
+                <Form.Control 
+                as='textarea'
+                value={textInput}
+                onChange={(e) => setTextInput(e.target.value)}
+                />
+              </Form>
+              <Button>Submit</Button>
+            </div>
+            <div className='cardRightDiv'>
+              <Form>
+                <Form.Group controlId="formFile" className="mb-3">
+                  <Form.Label>Audio Input</Form.Label>
+                  <Form.Control type="file" />
+                </Form.Group>
+              </Form>
+              <Button>Submit</Button>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+      <Card className='inputAndOutputCard'>
+        <Card.Header>
+          Response
+        </Card.Header>
+        <Card.Body>
+        
+        </Card.Body>
+      </Card>
     </div> 
     </>
   );
