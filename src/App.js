1ae515f4f7db0a33f99bf2a3e23b59ddbc65853c
Peter <phellwege1@gmail.com>
2024-04-03 16:04:20 -0600

    grimaldus openai voice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
-import react, { useState } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Card, Form } from 'react-bootstrap';
 import LoadingPage from './loading/LoadingPage'
-import React from 'react';
+import OpenAI from 'openai';
 
 function App() {
+  const openai = new OpenAI({apiKey:`${process.env.REACT_APP_GRIMALDUS_API_KEY}`, dangerouslyAllowBrowser: true})
   const [loading, setLoading] = useState(false);
-  const [textInput, setTextInput] = useState(null);
+  const [textInput, setTextInput] = useState('');
+  const [audioFileInput, setAudioFileInput] = useState(null);
+  const [finalText, setFinalText] = useState(null);
+
+  const [whisperTranscription, setWhisperTranscription] = useState(null)
+  const [gptGeneratedResponse, setGptGeneratedResponse] = useState(null);
+
+  const handleSubmitText = async (e) => {
+    e.preventDefault();
+    if(textInput.length < 1) {
+      return
+    }
+    try {
+      setLoading(true);
+      setFinalText(textInput);
+      await generateGPTResponse(finalText)
+    } catch (error) {
+      console.error(error)
+    }
+  };
+  // this is the brain of grimaldus
+  const generateGPTResponse = async (finalText) => {
+    try {
+      const gptResponse = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'system',
+                content: `
+                Reply to this as Grimaldus, the Reclusiarch of the Black Templars Chapter from Warhammer 40k. Speak in his authoritative and fervent tone, demonstrating his unwavering dedication to the Emperor and the righteous fury of the Black Templars. Keep your response to a single paragraph.
+                `
+            },
+            {
+                role: 'user',
+                content: `
+                    ${finalText}.
+                `
+            }
+        ],
+        temperature: 0.7,
+        max_tokens: 500,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        stop: "{}",
+    });
+    setLoading(false);
+    setGptGeneratedResponse(gptResponse.choices[0].message.content);
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
   return (
     <>
     {loading && (
@@ -23,14 +76,17 @@ function App() {
           <div className='cardInnerDiv'>
             <div className='cardLeftDiv'>
               <Form>
-                <Form.Label>Text Input</Form.Label>
+                <Form.Label>
+                  Text Input {textInput.length}/500
+                  </Form.Label>
                 <Form.Control 
                 as='textarea'
+                maxLength={500}
                 value={textInput}
                 onChange={(e) => setTextInput(e.target.value)}
                 />
               </Form>
-              <Button>Submit</Button>
+              <Button onClick={(e) => handleSubmitText(e)}>Submit</Button>
             </div>
             <div className='cardRightDiv'>
               <Form>
@@ -40,6 +96,7 @@ function App() {
                   type="file"
                   accept='audio/*'
                   />
+                  <Form.Text>File uploads are currently limited to 25 MB and the following input file types are supported: mp3, mp4, mpeg, mpga, m4a, wav, and webm.</Form.Text>
                 </Form.Group>
               </Form>
               <Button>Submit</Button>
@@ -52,7 +109,12 @@ function App() {
           Response
         </Card.Header>
         <Card.Body>
-        
+        {gptGeneratedResponse && (
+          <>
+          <h5>Grimaldus</h5>
+          {gptGeneratedResponse}
+          </>
+          )}
         </Card.Body>
       </Card>
     </div> 
