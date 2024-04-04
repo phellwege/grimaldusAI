4926fe26ab008ad2ff5885e26d04facfaf0729e9
Peter <phellwege1@gmail.com>
2024-04-04 10:33:24 -0600

    grimaldus vox

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const tts_url = `https://api.elevenlabs.io/v1/text-to-speech/${process.env.REACT_APP_VOICE_ID}/stream`;
   const [loading, setLoading] = useState(false);
   const [textInput, setTextInput] = useState('');
+  const [audioFile, setAudioFile] = useState(null);
   const [audioUrl, setAudioUrl] = useState('');
   const [whisperTranscription, setWhisperTranscription] = useState(null)
   const [gptGeneratedResponse, setGptGeneratedResponse] = useState(null);
@@ -65,8 +66,8 @@ function App() {
     }
   };
 
-  
-  const handleTheVoiceOfGrimaldus = () => {
+  // download
+  const handleTheVoiceOfGrimaldusDownload = () => {
     if (!gptGeneratedResponse) {
       console.error('No generated response to convert to speech');
       return;
@@ -110,52 +111,87 @@ function App() {
     });
   };
 
-  // this doesnt work yet.
-  // const handlePlayAudio = async () => {
-  //   if (!gptGeneratedResponse) {
-  //     console.error('No generated response to play');
-  //     return;
-  //   }
+  // playback
+  const handleTheVoiceOfGrimaldus = () => {
+    if (!gptGeneratedResponse) {
+      console.error('No generated response to convert to speech');
+      return;
+    }
+    setLoading(true)
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'xi-api-key': `${process.env.REACT_APP_XI_API_KEY}`
+      },
+      body: JSON.stringify({
+        "model_id": "eleven_multilingual_v2",
+        "text": gptGeneratedResponse,
+        "voice_settings": {
+          "stability": 0.5,
+          "similarity_boost": 0.75,
+          "style": 0.0,
+          "use_speaker_boost": true
+        },
+        "output_format": "mp3_44100_64" 
+      })
+    };
   
-  //   const headers = {
-  //     "Accept": "application/json",
-  //     "xi-api-key": `${process.env.REACT_APP_XI_API_KEY}`
-  //   };
-  //   const data = {
-  //     "text": gptGeneratedResponse,
-  //     "model_id": "eleven_multilingual_v2",
-  //     "voice_settings": {
-  //       "stability": 0.3,
-  //       "similarity_boost": 0.9,
-  //       "style": 0.0,
-  //       "use_speaker_boost": true
-  //     }
-  //   };
+    fetch(tts_url, options)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.blob();
+      })
+      .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const audioElement = new Audio(url);
+        setLoading(false)
+        audioElement.play().catch(error => {
+          console.error('Error playing audio:', error);
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+  
+  
+  // audio ingestion file blocked by cors
+  // const handleFileChange = (event) => {
+  //   setAudioFile(event.target.files[0]);
+  // };
+  // const handleTranscription = async () => {
+  //   if (!audioFile) return;
+
+  //   setLoading(true);
   //   try {
-  //     const response = await fetch(tts_url, {
+  //     const formData = new FormData();
+  //     formData.append('audio', audioFile);
+
+  //     const response = await fetch('https://api.openai.com/v1/speech/transcribe', {
   //       method: 'POST',
-  //       headers: headers,
-  //       body: JSON.stringify(data)
+  //       headers: {
+  //         Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+  //       },
+  //       body: formData,
   //     });
+
   //     if (!response.ok) {
-  //       throw new Error('Network response was not ok');
+  //       throw new Error('Failed to transcribe audio');
   //     }
-  //     const blob = await response.blob();
-  //     const url = window.URL.createObjectURL(blob);
-  
-  //     // Create an audio element
-  //     const audioElement = new Audio();
-  //     audioElement.src = url;
-  
-  //     // Play the audio
-  //     audioElement.play().catch(error => {
-  //       console.error('Error playing audio:', error);
-  //     });
+
+  //     const data = await response.json();
+  //     setTextInput(data.transcription);
+  //     generateGPTResponse()
+  //     // setTranscription('');
   //   } catch (error) {
-  //     console.error('Error:', error);
+  //     console.error('Error transcribing audio:', error);
+  //   } finally {
+  //     setLoading(false);
   //   }
   // };
-  
 
   return (
     <>
@@ -190,17 +226,20 @@ function App() {
                 Submit
               </Button>
             </div>
-            <div className='cardRightDiv'>
-              <Form>
+            
+            {/* <div className='cardRightDiv'> */}
+              {/* <Form>
                 <Form.Group controlId="formFile" className="mb-3">
                   <Form.Label>Audio Input</Form.Label>
                   <Form.Control 
                   type="file"
                   accept='audio/*'
+                  onChange={handleFileChange}
                   />
                   <Form.Text>File uploads are currently limited to 25 MB and the following input file types are supported: mp3, mp4, mpeg, mpga, m4a, wav, and webm.</Form.Text>
                 </Form.Group>
-              </Form>
+              </Form> */}
+
               {/* <div>
                 <h5>Or Try</h5>
                 <div 
@@ -214,8 +253,8 @@ function App() {
                   )}
                 </div>
               </div> */}
-              <Button>Submit</Button>
-            </div>
+              {/* <Button onClick={handleTranscription}>Submit</Button>
+            </div> */}
           </div>
         </Card.Body>
       </Card>
@@ -229,9 +268,14 @@ function App() {
           <h5>Grimaldus</h5>
           {gptGeneratedResponse}
           <br/>
-          <Button onClick={handleTheVoiceOfGrimaldus}>
-            Hear Me!
-          </Button>
+          <div className='cardButtonGroup'>
+            <Button onClick={handleTheVoiceOfGrimaldus}>
+              Hear Me!
+            </Button>
+            <Button onClick={handleTheVoiceOfGrimaldusDownload}>
+              Download My Holy Words
+            </Button>
+          </div>
           </>
           )}
         </Card.Body>
